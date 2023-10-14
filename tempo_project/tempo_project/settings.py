d06d8b533b3b80ebd682fa9af5ce997818240120
Cas <cssamonte@gmail.com>
2023-10-13 17:02:11 -0700

    removed debug=false in settings.py added line 119 for completeness

--- tempo_project/tempo_project/settings.py
+++ tempo_project/tempo_project/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-25u63%5jgx6n%jc8#)eh7z%cf8mib5*n%ruhzb#ikpy*x!k1d)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -115,7 +115,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "tempo_app" / "static"]
+
 
 
 # Default primary key field type
