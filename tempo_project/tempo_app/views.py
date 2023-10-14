b3a682d61d33af55ea1a4a956e63b9090dbbdf12
Jonathan <Jonnaa96@yahoo.com>
2023-10-13 19:46:54 -0600

    Starter artist detail page and base html

--- tempo_project/tempo_app/views.py
+++ tempo_project/tempo_app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-
+from .seed_artist import Artists
 
 def landing(request):
     return render(request, 'tempo_app/landing.html')
 
 def artist(request):
-    return render(request, 'tempo_app/artist.html')
+    return render(request, 'tempo_app/artist.html',{
+        'artist':Artists[0],
+    })
 
 def artist_api(request):
     return render(request, 'tempo_app/artist_api.html')
