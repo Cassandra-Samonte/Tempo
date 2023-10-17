06992ee5b1e2497353dcc0fb686ecfaf2bc829fe
Cas <cssamonte@gmail.com>
2023-10-17 12:04:00 -0700

    fixed merch view template

--- tempo_project/tempo_app/views.py
+++ tempo_project/tempo_app/views.py
@@ -117,4 +117,4 @@ def artist_api(request):
 # Merch
 def merch(request):
     merchs = Merch.objects.all()
-    return render(request, 'merch.html', {'merchs': merchs})
+    return render(request, 'tempo_app/merch.html', {'merchs': merchs})
