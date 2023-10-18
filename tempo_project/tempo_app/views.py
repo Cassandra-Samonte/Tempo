e525e8606325bb6397377ffc864005a62f963f01
Cas <cssamonte@gmail.com>
2023-10-18 09:37:06 -0700

    duplicate artist detail functions

--- tempo_project/tempo_app/views.py
+++ tempo_project/tempo_app/views.py
@@ -207,37 +207,37 @@ def callback(request):
 #     print()
 #     return redirect('player')
 
-# Artist detail
-def artist(request, artist_id):
-    # Get artist object matching name
-    # artist = Artist.objects.get(name='Drake')
-    artist = Artist.objects.get(id=artist_id)
+# # Artist detail
+# def artist(request, artist_name):
+#     # Get artist object matching name
+#     # artist = Artist.objects.get(name='Drake')
+#     artist = Artist.objects.get(id=artist_id)
 
     
-    # use main.py functions(funtions to use spotify api)
-    token = get_token()
-    result = search_for_artist(token, artist.name)
+#     # use main.py functions(funtions to use spotify api)
+#     token = get_token()
+#     result = search_for_artist(token, artist.name)
 
-    # Getting artist Id, necessary to get artist details
-    artist_id = result["id"]
+#     # Getting artist Id, necessary to get artist details
+#     artist_id = result["id"]
 
-    # Getting artist top tracks using api
-    songs = get_songs_by_artist(token, artist_id)
+#     # Getting artist top tracks using api
+#     songs = get_songs_by_artist(token, artist_id)
 
-    # Putting all the song names into a list
-    song_list = []
-    for song in songs:
-        song_list.append(song['name'])
+#     # Putting all the song names into a list
+#     song_list = []
+#     for song in songs:
+#         song_list.append(song['name'])
 
-    # Getting artist picture
-    image_url = result["images"][0]["url"]
-    print(image_url)
+#     # Getting artist picture
+#     image_url = result["images"][0]["url"]
+#     print(image_url)
 
-    return render(request, 'tempo_app/artist.html',{
-        'artist': artist.name,
-        'songs': song_list,
-        'image_url': image_url,
-    })
+#     return render(request, 'tempo_app/artist.html',{
+#         'artist': artist.name,
+#         'songs': song_list,
+#         'image_url': image_url,
+#     })
 
 # def artist_api(request):
 #     return render(request, 'tempo_app/artist_api.html')
