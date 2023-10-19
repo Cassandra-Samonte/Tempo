17b747e9af6dd105895d446f998af7614c32f758
Cas <cssamonte@gmail.com>
2023-10-19 10:47:40 -0700

    added merch photos and updated seed data

--- tempo_project/tempo_app/seed_artist.py
+++ tempo_project/tempo_app/seed_artist.py
@@ -35,294 +35,308 @@ Merchs = [
         'item':'Hoodie',
         'description':'Classic Hoodie',
         'price':158,
-        'image':'https://res.cloudinary.com/shogun-frontend/image/fetch/f_auto,q_auto:eco,c_limit,w_1080/https://f.shgcdn.com/9fc654ae-6de0-4645-a5b9-c5d1574fa927/',
+        'image':'merch/1.png',
         'name':'Drake'
         },
     {
         'item':'Tee',
         'description':'Classic Tee',
         'price':58,
-        'image':'https://res.cloudinary.com/shogun-frontend/image/fetch/f_auto,q_auto:eco,c_limit,w_1080/https://f.shgcdn.com/c6e96df6-1709-4762-b6f7-8704d86aea4f/',
+        'image':'merch/2.png',
         'name':'Drake'
         },
     {
         'item':'Sweatpants',
         'description':'Classic Relaxed Fit Sweatpants',
         'price':128,
-        'image':'https://res.cloudinary.com/shogun-frontend/image/fetch/f_auto,q_auto:eco,c_limit,w_1080/https://f.shgcdn.com/a4a85801-cfad-43d9-8414-468f865190ef/',
+        'image':'merch/3.png',
         'name':'Drake'
         },
     {
         'item':'Sportcap',
         'description':'Classic Sportcap',
         'price':58,
-        'image':'https://res.cloudinary.com/shogun-frontend/image/fetch/f_auto,q_auto:eco,c_limit,w_1080/https://f.shgcdn.com/6ec143c8-9d69-4465-978f-66828a080549/',
+        'image':'merch/4.png',
         'name':'Drake'
+        },
+    {
+        'item':'T-Shirt',
+        'description':'Beyonce Renaissance World Tour Merch Disco Cowboy Hat T-Shirt',
+        'price':32.90,
+        'image':'merch/5.png',
+        'name':'Beyoncé'
+        },
+    {
+        'item':'Sweatshirt',
+        'description':'Beyonce Renaissance World Tour Outside Sweatshirt',
+        'price':55,
+        'image':'merch/6.png',
+        'name':'Beyoncé'
         },
     {
         'item':'Sweatshirt',
         'description':'Beyonce Renaissance World Tour Chroma Key Sweatshirt',
         'price':69,
-        'image':'https://www.beyoncesstore.com/wp-content/uploads/image/beyoncesstore/images/131653ca6808aa3ab509968f384a3b93.jpg',
+        'image':'merch/7.png',
         'name':'Beyoncé'
         },
     {
         'item':'Hoodie',
         'description':'Beyonce All Up In Your Mind Face Hoodie',
         'price':65,
-        'image':'https://www.beyoncesstore.com/wp-content/uploads/image/beyoncesstore/images/84a025b24fcc3ffb396f00cae4ba9636.png',
+        'image':'merch/8.png',
         'name':'Beyoncé'
         },
     {
         'item':'Hoodie',
         'description':'1989 (Taylor''s Version) Blue Tie Dye Hoodie',
         'price':74.89,
-        'image':'https://store.taylorswift.com/cdn/shop/files/zx97wvo571dX5UUuG83SIsBJnFHcLKLa_1_1000x.png?v=1696604268',
+        'image':'merch/9.png',
         'name':'Taylor Swift'
         },
     {
         'item':'T-Shirt',
         'description':'Taylor Swift The Eras International Tour Mineral Wash Gray T-Shirt',
         'price':45,
-        'image':'https://store.taylorswift.com/cdn/shop/files/ysp064iNzUTUZv89kjAj_1000x.png?v=1687282799',
+        'image':'merch/10.png',
         'name':'Taylor Swift'
         },
     {
         'item':'Hat',
         'description':'1989 (Taylor''s Version) Taupe Hat',
         'price':39.89,
-        'image':'https://store.taylorswift.com/cdn/shop/files/mylx580l8N5PkmWaWWv5WLwHwiHh0D9f_1_1000x.png?v=1696604414',
+        'image':'merch/11.png',
         'name':'Taylor Swift'
         },
     {
         'item':'Vinyl',
         'description':'1989 (Taylor''s Version) Vinyl',
         'price':31.89,
-        'image':'https://store.taylorswift.com/cdn/shop/files/48qJrS3K8HaY5H1iz4dEBgP5HibRoev2_1000x.png?v=1695239977',
+        'image':'merch/12.png',
         'name':'Taylor Swift'
         },
     {
         'item':'T-Shirt',
         'description':'Script Forest Green T-Shirt',
         'price':25,
-        'image':'https://www.hellomerch.com/cdn/shop/products/Her_s21024.png?v=1659393210&width=1000',
+        'image':'merch/13.png',
         'name':f"Her's"
         },
     {
         'item':'Hat',
         'description':'Script Dino Green Dad Hat',
         'price':25,
-        'image':'https://www.hellomerch.com/cdn/shop/products/Her_s71024.png?v=1614028624&width=1000',
+        'image':'merch/14.png',
         'name':f"Her's"
         },
     {
         'item':'Vinyl',
         'description':'Songs of Hers 12" Vinyl',
         'price':25,
-        'image':'https://www.hellomerch.com/cdn/shop/products/Hers-SongsofHers-LP-MAIN.png?v=1614028514&width=1000',
+        'image':'merch/15.png',
         'name':f"Her's"
         },
     {
         'item':'Vinyl',
         'description':'Invitation to Hers 12" Vinyl',
         'price':25,
-        'image':'https://www.hellomerch.com/cdn/shop/products/Hers-InvitationToHers-LP-MAIN.png?v=1614028606&width=1000',
+        'image':'merch/16.png',
         'name':f"Her's"
         },
     {
         'item':'T-Shirt',
         'description':'HEADSPACE LONGSLEEVE SHIRT, BLACK',
         'price':35,
-        'image':'https://store.badsuns.com/cdn/shop/products/BADSUNS_HeadSpace-LS_2048x2048.png?v=1621547582',
+        'image':'merch/17.png',
         'name':'Bad Suns'
         },
     {
         'item':'T-Shirt',
         'description':'LOGO RINGER TEE, BLACK',
         'price':25,
-        'image':'https://store.badsuns.com/cdn/shop/products/BADSUNS_Logo-Ringer_2048x2048.png?v=1621446754',
+        'image':'merch/18.png',
         'name':'Bad Suns'
         },
     {
         'item':'Windbreaker',
         'description':'APOCALYPSE WHENEVER OVAL LOGO WINDBREAKER, BLACK',
         'price':75,
-        'image':'https://store.badsuns.com/cdn/shop/products/BADSUNS_windbreaker-back_2048x2048.png?v=1669917433',
+        'image':'merch/19.png',
         'name':'Bad Suns'
         },
     {
         'item':'Vinyl',
         'description':'MYSTIC TRUTH LP',
         'price':22,
-        'image':'https://store.badsuns.com/cdn/shop/files/BadSuns_MysticTruthVinyl_2048x2048.png?v=1682438473',
+        'image':'merch/20.png',
         'name':'Bad Suns'
         },
     {
         'item':'Crewneck',
         'description':'CATCH A FIRE TIE DYE CREWNECK',
         'price':65,
-        'image':'https://shop.bobmarley.com/cdn/shop/products/tie-dye-crewneck-brown-front.png?v=1691429051',
+        'image':'merch/21.png',
         'name':'Bob Marley & The Wailers'
         },
     {
         'item':'T-Shirt',
         'description':'CATCH A FIRE TIE DYE T-SHIRT',
         'price':35,
-        'image':'https://shop.bobmarley.com/cdn/shop/products/catch-a-fire-tie-dye-tee.png?v=1691429062',
+        'image':'merch/22.png',
         'name':'Bob Marley & The Wailers'
         },
     {
         'item':'Vinyl',
         'description':'AFRICA UNITE LIMITED EDITION RED LP',
         'price':32.99,
-        'image':'https://shop.bobmarley.com/cdn/shop/products/AU_RED_VINYL.png?v=1687963469',
+        'image':'merch/23.png',
         'name':'Bob Marley & The Wailers'
         },
     {
-        'item':'Hoodie',
-        'description':'SUNSHINE BLACK HOODIE',
-        'price':60,
-        'image':'https://shop.bobmarley.com/cdn/shop/files/Sunshine-Hoodie.png?v=1691607929',
+        'item':'Vinyl',
+        'description':'KAYA (TUFF GONG JAMAICA PRESSING)',
+        'price':34.98,
+        'image':'merch/24.png',
         'name':'Bob Marley & The Wailers'
         },
     {
         'item':'Crewneck',
         'description':'Meet the Moonlight Ivory Crewneck Sweatshirt',
         'price':70,
-        'image':'https://store.jackjohnsonmusic.com/cdn/shop/products/MTMWHITERAGLANFRONT_grande.png?v=1661474060',
+        'image':'merch/25.png',
         'name':'Jack Johnson'
         },
     {
         'item':'Tank Top',
         'description':'Moon Phases Cactus Tank Top',
         'price':45,
-        'image':'https://store.jackjohnsonmusic.com/cdn/shop/products/MTMGREENTANKFORNT_be239656-88f4-4ea0-ac8a-7edd73380fe5_grande.png?v=1669143537',
+        'image':'merch/26.png',
         'name':'Jack Johnson'
         },
     {
         'item':'Hat',
         'description':'Meet The Moonlight Patch Hat',
         'price':30,
-        'image':'https://store.jackjohnsonmusic.com/cdn/shop/products/MTMCHARCOALCAP_grande.png?v=1684438155',
+        'image':'merch/27.png',
         'name':'Jack Johnson'
         },
     {
         'item':'Vinyl',
         'description':'In Between Dreams - Vinyl',
         'price':35,
-        'image':'https://store.jackjohnsonmusic.com/cdn/shop/files/LP-IBD_grande.png?v=1695767137',
+        'image':'merch/28.png',
         'name':'Jack Johnson'
         },
     {
         'item':'Vinyl',
         'description':'VINYL 12" EP "ANTARES"',
         'price':20,
-        'image':'https://static.wixstatic.com/media/b5c829_cea17c676d0849f3a1a6f87ff9bff46c~mv2.jpg/v1/fill/w_500,h_500,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/b5c829_cea17c676d0849f3a1a6f87ff9bff46c~mv2.jpg',
+        'image':'merch/29.png',
         'name':'Ours Samplus'
         },
     {
         'item':'CD',
         'description':'Ours Samplus - One Day One Beat',
         'price':10,
-        'image':'https://static.wixstatic.com/media/b5c829_61e18daa230843c5bb3a3d60ccb5560a~mv2.jpg/v1/fill/w_500,h_500,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/b5c829_61e18daa230843c5bb3a3d60ccb5560a~mv2.jpg',
+        'image':'merch/30.png',
         'name':'Ours Samplus'
         },
     {
         'item':'CD',
         'description':'PACK 6xCD',
         'price':30,
-        'image':'https://static.wixstatic.com/media/b5c829_6e84147096e54e73ae359c4f2eb76dfc~mv2.jpg/v1/fill/w_500,h_500,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/b5c829_6e84147096e54e73ae359c4f2eb76dfc~mv2.jpg',
+        'image':'merch/31.png',
         'name':'Ours Samplus'
         },
     {
         'item':'Vinyl',
         'description':'2x VINYL 12" Album "LAST FRONTIER"',
         'price':23,
-        'image':'https://static.wixstatic.com/media/b5c829_be0b6ff703d14a1cbaec176a782613a2~mv2_d_2572_2572_s_4_2.jpg/v1/fill/w_500,h_500,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/b5c829_be0b6ff703d14a1cbaec176a782613a2~mv2_d_2572_2572_s_4_2.jpg',
+        'image':'merch/32.png',
         'name':'Ours Samplus'
         },
     {
         'item':'T-Shirt',
         'description':'YOU''VE GOT TO LEARN CONCERT T-SHIRT (BLACK)',
         'price':35,
-        'image':'https://shop.ninasimone.com/cdn/shop/files/NinaBlackTee.png?v=1689626462',
+        'image':'merch/33.png',
+        'name':'Nina Simone'
+        },
+    {
+        'item':'T-Shirt',
+        'description':'NINA SIMONE SILHOUETTE BLACK T-SHIRT',
+        'price':35,
+        'image':'merch/34.png',
         'name':'Nina Simone'
         },
     {
         'item':'Vinyl',
         'description':'YOU''VE GOT TO LEARN COLOR LP',
         'price':25.98,
-        'image':'https://shop.ninasimone.com/cdn/shop/products/High-Priestess-Of-Soul-LP.png?v=1678211645',
+        'image':'merch/35.png',
         'name':'Nina Simone'
         },
     {
         'item':'Vinyl',
         'description':'HIGH PRIESTESS OF SOUL LP',
         'price':24.98,
-        'image':'https://shop.ninasimone.com/cdn/shop/files/NinaSimoneYou_veGotToLearnColorLP.png?v=1686067492',
-        'name':'Nina Simone'
-        },
-    {
-        'item':'Vinyl',
-        'description':'I PUT A SPELL ON YOU (MASTERCUT EDITION)',
-        'price':499.98,
-        'image':'https://shop.ninasimone.com/cdn/shop/products/NINAS3_c8db0e0e-af5a-4117-8674-b3f2b94cccca.png?v=1675264832',
+        'image':'merch/36.png',
         'name':'Nina Simone'
         },
     {
         'item':'Hoodie',
         'description':'So Icy Boyz The New 1017 Crew Hoodie',
         'price':60,
-        'image':'https://shop.thenew1017records.com/dw/image/v2/BHCC_PRD/on/demandware.static/-/Sites-warner-master/default/dwa111cfeb/pdp-img/guccihoodie3.png?sw=550&sh=550&sm=fit',
+        'image':'merch/37.png',
         'name':'Gucci Mane'
         },
     {
         'item':'Sweatshirt',
         'description':'Brr Holiday Cone Sweatshirt',
         'price':50,
-        'image':'https://shop.thenew1017records.com/dw/image/v2/BHCC_PRD/on/demandware.static/-/Sites-warner-master/default/dw6fa2fd5c/pdp-img/1017/7dba43a4-6009-4d7a-8af3-cbf509c58f5e.png?sw=550&sh=550&sm=fit',
+        'image':'merch/38.png',
         'name':'Gucci Mane'
         },
     {
         'item':'Vinyl',
         'description':'Breath Of Fresh Air Vinyl',
         'price':24.98,
-        'image':'https://shop.thenew1017records.com/dw/image/v2/BHCC_PRD/on/demandware.static/-/Sites-warner-master/default/dw2735b868/GUCCI%20MANE/Retail_WhiteVinyl.png?sw=550&sh=550&sm=fit',
+        'image':'merch/39.png',
         'name':'Gucci Mane'
         },
     {
         'item':'CD',
         'description':'Breath Of Breath of Fresh Air Vinyl | The New 1017 Official StoreFresh Air Signed Art Card CD',
         'price':18.98,
-        'image':'https://shop.thenew1017records.com/dw/image/v2/BHCC_PRD/on/demandware.static/-/Sites-warner-master/default/dwc75ed2f3/GUCCI%20MANE/BoFA_CDSigned.png?sw=550&sh=550&sm=fit',
+        'image':'merch/40.png',
         'name':'Gucci Mane'
         },
     {
         'item':'T-Shirt',
         'description':'400 BLOCK BUSTER T-SHIRT (CREAM)',
         'price':35,
-        'image':'https://4hunnid.com/cdn/shop/files/400BLOCKBUSTERT-SHIRT_CREAM_-Back.jpg?v=1692298150"',
+        'image':'merch/41.png',
         'name':'YG'
         },
     {
         'item':'Hoodie',
         'description':'4 BLOCK LOGO HOODIE (BROWN)',
         'price':90,
-        'image':'https://4hunnid.com/cdn/shop/products/4BLOCKLOGOHOODIE_BROWN_-Front.jpg?v=1680650617"',
+        'image':'merch/42.png',
         'name':'YG'
         },
     {
         'item':'Sweatpants',
         'description':'4HUNNID HIT UP SWEATPANTS (TRAVERTINE)',
         'price':120,
-        'image':'https://4hunnid.com/cdn/shop/products/4HUNNIDKOMFORTJOGGER_TRAVERTINE_-Front.jpg?v=1680589642"',
+        'image':'merch/43.png',
         'name':'YG'
         },
     {
         'item':'Snapback',
         'description':'FH SNAPBACK (BROWN)',
         'price':45,
-        'image':'https://4hunnid.com/cdn/shop/products/FHSNAPBACK_BROWN_-Front.jpg?v=1680589654',
+        'image':'merch/44.png',
         'name':'YG'
         },
 ]
