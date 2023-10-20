f48a6c1d6b67da49a90ec955845b760f21078adf
secretivo <angelica.erazo22@gmail.com>
2023-10-20 15:43:23 -0700

    reverse edits

--- tempo_project/tempo_project/wsgi.py
+++ tempo_project/tempo_project/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tempo_project.settings')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spotify_clone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tempo_project.settings')
+
 
 application = get_wsgi_application()
