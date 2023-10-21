ff73af71fcd115c4ea8b0ad1427d4af4d909e1ec
Jonathan <Jonnaa96@yahoo.com>
2023-10-21 14:39:56 -0600

    added auth with pkce flow

--- tempo_app/views.py
+++ tempo_app/views.py
@@ -18,7 +18,8 @@ import urllib.parse
 
 
 class StoredInfo:
-    redirect_uri='https://tempoproject-1ccff4d917c0.herokuapp.com/callback'
+    # redirect_uri='https://tempoproject-1ccff4d917c0.herokuapp.com/callback'
+    redirect_uri='http://localhost:8000/callback'
     access_token = ''
     refresh_token = ''
 
@@ -150,61 +151,107 @@ def artist_api(request):
 # https://developer.spotify.com/documentation/web-api/tutorials/code-flow
 # the documentation is in JS and uses express
 # converting the code was a major challenge
+# def login(request):
+#     # var to specify how many characters the random string should be
+#     N = 16
+#     # state is a optional param which provides added security
+#     # This provides protection against attacks such as cross-site request forgery
+#     state = ''.join(random.choices(string.ascii_uppercase +
+#                              string.digits, k=N))
+#     # Scope are the permissions we want the user to authorize(can add more)
+#     # https://developer.spotify.com/documentation/web-api/concepts/scopes
+#     scope = 'user-read-private user-read-email user-top-read user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming user-read-playback-position';
+#     # convert an object to url query form and save it
+#     query_string = urllib.parse.urlencode({
+#         'response_type': 'code',
+#         'client_id': client_id,
+#         'scope':scope,
+#         'redirect_uri':StoredInfo.redirect_uri,
+#         'state':state,
+#     })
+#     # redirect to the page that asks the user to authorize
+#     # once authorized(or cancelled), redirects to redirect uri(stored here, but also saved on spotify app dashboard)
+#     return redirect('https://accounts.spotify.com/authorize?'+query_string)
+codeVerifier = generateRandomString(128)
 def login(request):
-    # var to specify how many characters the random string should be
-    N = 16
-    # state is a optional param which provides added security
-    # This provides protection against attacks such as cross-site request forgery
-    state = ''.join(random.choices(string.ascii_uppercase +
-                             string.digits, k=N))
-    # Scope are the permissions we want the user to authorize(can add more)
-    # https://developer.spotify.com/documentation/web-api/concepts/scopes
+    codeVerifier = generateRandomString(128)
+    # codeVerifier = 'tTipOAamz8fcpEgKTRZk3L5Ps6aMqgQv1CV8mYMxvL0Zxajoh0v0ImqOwJpHGRuPyt5qZocKsi1IlIwTyXQJSjILvYnsxzwj3bWQHYzrvHENGcSDWbadYhN8vkiN4Upb'
+    # print('Code Verifier: '+ codeVerifier)
+    codeChallenge = generateCodeChallenge(codeVerifier=codeVerifier)
+    # print("Code Challenge: "+codeChallenge)
+    state = generateRandomString(16)
     scope = 'user-read-private user-read-email user-top-read user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming user-read-playback-position';
-    # convert an object to url query form and save it
+    localStorage.setItem('code_verifier', codeVerifier)
     query_string = urllib.parse.urlencode({
-        'response_type': 'code',
+        'response_type':'code',
         'client_id': client_id,
         'scope':scope,
         'redirect_uri':StoredInfo.redirect_uri,
         'state':state,
+        'code_challenge_method':'S256',
+        'code_challenge':codeChallenge
     })
-    # redirect to the page that asks the user to authorize
-    # once authorized(or cancelled), redirects to redirect uri(stored here, but also saved on spotify app dashboard)
+    # print('Code Challenge: '+codeChallenge)
+    # print('Code Verifier: '+codeVerifier)
+    # print('Args: ' + query_string)
     return redirect('https://accounts.spotify.com/authorize?'+query_string)
 
-# https://developer.spotify.com/documentation/web-api/tutorials/code-flow
 def callback(request):
     code = request.GET['code']
-    state = request.GET['state']
+    codeVerifier = localStorage.getItem('code_verifier')
+    # print('Local Code Verifier: '+ codeVerifier)
 
-    # First concatenate client id and client secret(important to have ":")
-    auth_string = client_id + ":" + client_secret
+    body = {
+        'grant_type': 'authorization_code',
+        'code':code,
+        'redirect_uri':StoredInfo.redirect_uri,
+        'client_id':client_id,
+        'code_verifier':codeVerifier
+    }
+    headers={
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    url = 'https://accounts.spotify.com/api/token'
 
-    # Encode the concatenated string
-    auth_bytes = auth_string.encode("utf-8")
+    result = post(url=url, headers=headers, data=body)
+    json_result = json.loads(result.content)
+    StoredInfo.access_token = json_result['access_token']
+    StoredInfo.refresh_token = json_result['refresh_token']
 
-    # Encode using base 64
-    #   base64... returns a base64 object and then it's converted into a string
-    auth_base64 = str(base64.b64encode(auth_bytes),"utf-8")
-
-    if state==None:
-        return redirect('login'+urllib.parse.urlencode({'error':'state_mismatch'}))
-    else:
-        url='https://accounts.spotify.com/api/token'
-        form = {
-            'code':code,
-            'redirect_uri':StoredInfo.redirect_uri,
-            'grant_type':'authorization_code',
-            }
-        headers = {
-            "Authorization":"Basic "+ auth_base64,
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
-        result = post(url, headers=headers, data=form)
-        json_result = json.loads(result.content)
-        StoredInfo.access_token = json_result['access_token']
-        StoredInfo.refresh_token = json_result['refresh_token']
     return redirect('landing')
+# # https://developer.spotify.com/documentation/web-api/tutorials/code-flow
+# def callback(request):
+#     code = request.GET['code']
+#     state = request.GET['state']
+
+#     # First concatenate client id and client secret(important to have ":")
+#     auth_string = client_id + ":" + client_secret
+
+#     # Encode the concatenated string
+#     auth_bytes = auth_string.encode("utf-8")
+
+#     # Encode using base 64
+#     #   base64... returns a base64 object and then it's converted into a string
+#     auth_base64 = str(base64.b64encode(auth_bytes),"utf-8")
+
+#     if state==None:
+#         return redirect('login'+urllib.parse.urlencode({'error':'state_mismatch'}))
+#     else:
+#         url='https://accounts.spotify.com/api/token'
+#         form = {
+#             'code':code,
+#             'redirect_uri':StoredInfo.redirect_uri,
+#             'grant_type':'authorization_code',
+#             }
+#         headers = {
+#             "Authorization":"Basic "+ auth_base64,
+#             "Content-Type": "application/x-www-form-urlencoded"
+#         }
+#         result = post(url, headers=headers, data=form)
+#         json_result = json.loads(result.content)
+#         StoredInfo.access_token = json_result['access_token']
+#         StoredInfo.refresh_token = json_result['refresh_token']
+#     return redirect('landing')
 
 
 def artist_search(request):
